43668ff0233fb1d796ac36ce46b535404f5710f0
Pawel Bartusiak <21136755+uFloppyDisk@users.noreply.github.com>
2024-10-23 20:04:36 -0700

    feat(state): add GetStateValue func;

--- src/state.lua
+++ src/state.lua
@@ -1,3 +1,12 @@
+---@enum (key) StateKeyRef
+local keyRef = {
+  ["UserShell"] = "selected_shell",
+  ["UserVariant"] = "selected_variant",
+  ["UserHeading"] = "selected_attack_heading",
+  ["UserAngle"] = "selected_attack_angle",
+  ["UserAccuracy"] = "shell_inaccuracy",
+}
+
 STATES = {
     enabled = false,
     fire = false,
@@ -36,6 +45,29 @@ STATES.tactical = {
 
 local DEFAULT_STATE = STATES
 
+local function parseKey(key)
+  return FdSplitString(key, ".")
+end
+
 function ResetToDefaultState()
   STATES = DEFAULT_STATE
+end
+
+---Get value from state table
+---@param keyOrKeyRef StateKeyRef|string
+---@return any
+function GetStateValue(keyOrKeyRef)
+  local key = keyOrKeyRef
+  if keyRef[keyOrKeyRef] ~= nil then key = keyRef[keyOrKeyRef] end
+
+  local path = parseKey(key)
+
+  local head = STATES
+  for _, seg in ipairs(path) do
+    head = head[seg]
+
+    if head == nil then return nil end
+  end
+
+  return head
 end
