1ef566bb139b619165112efc7c6989638d2937bf
Pawel Bartusiak <21136755+uFloppyDisk@users.noreply.github.com>
2026-03-20 19:04:46 -0700

    refactor: move client draw/tick;

--- src/server/main.lua
+++ src/server/main.lua
@@ -3,4 +3,10 @@ function server.init()
 
     RegisterTool("ordnance", "Ordnance [FD]", "MOD/assets/vox/radio.xml")
     SetBool("game.tool.ordnance.enabled", true)
+
+    G_PHYSICS_ITERATIONS = 2 ^ (CfgGetValue("G_PHYSICS_ITERATIONS") or 4)
+
+    if G_DEV then
+        SetPlayerSpawnTool("ordnance")
+    end
 end
